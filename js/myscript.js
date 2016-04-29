2d4dca9d9c03b0beac8e7b9277891ebc83e41348
tainea <tainea@local>
2016-04-29 16:05:59 +0200

    validate url

--- js/myscript.js
+++ js/myscript.js
@@ -7,7 +7,8 @@ function
 init()
 {
 	// enable SLICK
-	$('#carousel').slick({
+	$('#carousel').slick(
+	{
 		'infinite': true,
 		'autoplay': true,
 		'autoplaySpeed': 3000,
@@ -17,35 +18,22 @@ init()
 	});
 
 	// enable FORM VALIDATION
-	$('#enter-form').validate({
+	$('#enter-form').validate(
+	{
 		rules: {
 			email: "required",
     			name: "required",
     			discription: "required",
-    			pic: "required"
+			pic: {
+				required: true,
+      				url: true
+			}
     		},
 			
-    		submitHandler: function(form) {
+    		submitHandler: function(form)
+		{
     			alert("C'est OK ça part !!");
     			form.submit();
     		}
 	});
-
-/*
-		function ValidURL(str) {
-			var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    			'((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    			'((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    			'(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    			'(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    			'(\#[-a-z\d_]*)?$','i'); // fragment locater
-  			if(!pattern.test(str)) {
-    				alert("Please enter a valid URL.");
-    				return false;
-    			} else {
-				return true;
-			}
-		}
-	});
-*/
 }
