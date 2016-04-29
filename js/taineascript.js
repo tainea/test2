c8230b538a78b18009d7bea2b58a06cf9dd13c39
tainea <tainea@local>
2016-04-29 14:00:29 +0200

    First pages commit

---
+++ js/taineascript.js
@@ -0,0 +1,35 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+
+$("#enter-form").validate({
+	rules: {
+	email: "required",
+    name: "required",
+    discription: "required",
+    pic: "required"
+    },
+			
+    submitHandler: function(form){
+    alert("C'est OK ça part !!");
+    form.submit();
+    }
+
+    function ValidURL(str) {
+  	var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  	if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+    
+  	} else {
+    return true;
+  }
+}
+});
